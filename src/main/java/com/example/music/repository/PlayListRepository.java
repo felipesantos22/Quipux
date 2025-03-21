a91d36ea3438b1604c83e95cb4907cc45278542d
felipesantos22 <felipevs2018@gmail.com>
2025-03-21 10:36:42 -0300

    insert model playlist

---
+++ src/main/java/com/example/music/repository/PlayListRepository.java
@@ -0,0 +1,4 @@
+package com.example.music.repository;
+
+public interface PlayListRepository {
+}
--- src/main/java/com/example/music/service/PlayListService.java
+++ src/main/java/com/example/music/service/PlayListService.java
@@ -1,4 +1,33 @@
 package com.example.music.service;
 
-public class PlayListService {
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlaylistService {
+
+    private final PlaylistRepository playlistRepository;
+
+    public PlaylistService(PlaylistRepository playlistRepository) {
+        this.playlistRepository = playlistRepository;
+    }
+
+    public Playlist createPlaylist(Playlist playlist) {
+        return playlistRepository.save(playlist);
+    }
+
+    public List<Playlist> getAllPlaylists() {
+        return playlistRepository.findAll();
+    }
+
+    public Optional<Playlist> getPlaylistById(Long id) {
+        return playlistRepository.findById(id);
+    }
+
+    public boolean deletePlaylist(Long id) {
+        if (playlistRepository.existsById(id)) {
+            playlistRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
