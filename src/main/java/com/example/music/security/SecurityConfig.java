1ac7adb663b6642230b9743d68ca108a36c7c46f
felipesantos22 <felipevs2018@gmail.com>
2025-03-21 15:56:17 -0300

    change spring security

--- src/main/java/com/example/music/security/SecurityConfig.java
+++ src/main/java/com/example/music/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.music.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,8 +12,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests.anyRequest().permitAll());
-
+                        authorizeRequests.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
